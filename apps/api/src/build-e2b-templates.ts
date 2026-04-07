b96bd59265dfbc748de956f0a3423bf85c4da438
Code Monkey <cuddly.donation413@anonaddy.me>
2026-04-07 16:55:39 -0700

    Install Docker in E2B template

--- apps/api/src/build-e2b-templates.ts
+++ apps/api/src/build-e2b-templates.ts
@@ -6,6 +6,7 @@ import { Template, defaultBuildLogger } from 'e2b';
 const template = Template()
   .fromBaseImage()
   .aptInstall('gh')
+  .runCmd('curl -fsSL https://get.docker.com | sudo sh')
 
 async function main() {
   await Template.build(template, 'small', {
